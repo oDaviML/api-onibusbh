3376ac3fbc5c09f9336ea90beb662242a548d3f0
odaviml <davimartinslage2005@gmail.com>
2024-11-07 21:09:16 -0300

    [FIX] Remoção de variável não utilizada

--- src/main/java/com/dmware/api_onibusbh/services/DicionarioService.java
+++ src/main/java/com/dmware/api_onibusbh/services/DicionarioService.java
@@ -1,5 +1,10 @@
 package com.dmware.api_onibusbh.services;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeToken;
 import org.slf4j.Logger;
@@ -7,15 +12,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
 import com.dmware.api_onibusbh.dto.DicionarioDTO;
 import com.dmware.api_onibusbh.entities.DicionarioEntity;
 import com.dmware.api_onibusbh.repositories.DicionarioRepository;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Service
 public class DicionarioService {
@@ -27,7 +26,6 @@ public class DicionarioService {
       @Autowired
       private APIService apiService;
 
-      ObjectMapper objectMapper = new ObjectMapper();
       private static final Logger logger = LoggerFactory.getLogger(LinhasService.class);
 
       public List<DicionarioDTO> fetchDicionarios() {
