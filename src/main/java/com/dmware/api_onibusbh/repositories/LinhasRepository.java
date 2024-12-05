f847bb8d225b0aab3c36b16d374e7db0f4d4eb77
odaviml <davimartinslage2005@gmail.com>
2024-12-05 09:58:49 -0300

    feat: Alteração no repository

--- src/main/java/com/dmware/api_onibusbh/repositories/LinhasRepository.java
+++ src/main/java/com/dmware/api_onibusbh/repositories/LinhasRepository.java
@@ -1,6 +1,5 @@
 package com.dmware.api_onibusbh.repositories;
 
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -10,7 +9,5 @@ import com.dmware.api_onibusbh.entities.LinhaEntity;
 public interface LinhasRepository extends MongoRepository<LinhaEntity, String> {
 
       public Optional<LinhaEntity> findByNumeroLinha(Integer numeroLinha);
-
-      public Optional<List<LinhaEntity>> findByLinhaContaining(String linha);
 
 }
