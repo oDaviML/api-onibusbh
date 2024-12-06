ec47f828a05bb9e8aacd93d0e19a9ef66c5ced93
Davi Martins Lage <davimartinslage2005@gmail.com>
2024-12-05 22:32:02 -0300

    feat: Configurado o CORS

--- src/main/java/com/dmware/api_onibusbh/ApiOnibusbhApplication.java
+++ src/main/java/com/dmware/api_onibusbh/ApiOnibusbhApplication.java
@@ -6,9 +6,11 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 @EnableScheduling
+@CrossOrigin
 @OpenAPIDefinition(info = @Info(title = "API Onibus BH", version = "1.0", description = "API para consulta de dados relacionados aos onibus de Belo Horizonte"))
 public class ApiOnibusbhApplication {
 
