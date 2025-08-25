fa4e4e83119bda4ce8b69362c0566e4b2ea211fa
Davi Martins Lage <davimartinslage2005@gmail.com>
2025-08-24 21:10:10 -0300

    Refactor: Simplify WebClient default headers

--- src/main/java/com/dmware/api_onibusbh/config/WebClientConfig.java
+++ src/main/java/com/dmware/api_onibusbh/config/WebClientConfig.java
@@ -10,13 +10,7 @@ public class WebClientConfig {
     @Bean
     public WebClient webClient() {
         return WebClient.builder()
-                .defaultHeaders(headers -> {
-                    headers.add(HttpHeaders.USER_AGENT, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36");
-                    headers.add(HttpHeaders.ACCEPT, "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9");
-                    headers.add(HttpHeaders.ACCEPT_LANGUAGE, "en-US,en;q=0.9");
-                    headers.add(HttpHeaders.ACCEPT_ENCODING, "gzip, deflate, br");
-                    headers.add(HttpHeaders.CONNECTION, "keep-alive");
-                })
+                .defaultHeader(HttpHeaders.USER_AGENT, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36")
                 .build();
     }
 }
