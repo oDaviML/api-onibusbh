6bcf68439f4e359c2910f16c36237e5a0e63d8eb
Davi Martins Lage <davimartinslage2005@gmail.com>
2025-08-26 22:30:20 -0300

    Feat: Add default headers for WebClient to improve request handling

--- src/main/java/com/dmware/api_onibusbh/config/WebClientConfig.java
+++ src/main/java/com/dmware/api_onibusbh/config/WebClientConfig.java
@@ -17,6 +17,8 @@ public class WebClientConfig {
                 .build();
 
         return WebClient.builder()
+                .defaultHeader(HttpHeaders.USER_AGENT, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36")
+                .defaultHeader(HttpHeaders.REFERER, "https://dados.pbh.gov.br/")
                 .exchangeStrategies(strategies)
                 .build();
     }
